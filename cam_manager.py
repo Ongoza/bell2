1debcda27f91a50af9ea888be9756f49388c609f
oleg@ongoza.com <oleg@ongoza.com>
2018-04-08 19:37:44 -0700

    modified:   cam_manager.py 	modified:   camera.py 	modified:   config/camConfig.json

--- cam_manager.py
+++ cam_manager.py
@@ -1,35 +1,72 @@
 import threading
 import time
+import os
+import logging
+import json
+import camera
 
-theVar = 0
 threads = []
 
 
-class MyThread(threading.Thread):
-    # Override Thread's __init__ method to accept the parameters needed:
-    def __init__(self, n, ip):
-        print("start " + ip)
-        self.n = n
-        self.ip = ip
-        threading.Thread.__init__(self)
-
-    def kill(self):
-        print('kill:', self.ip)
-        self.killed = True
+class ServiceExit(Exception):
+    """
+    Custom exception which is used to trigger the clean exit
+    of all running threads and the main program.
+    """
+    pass
 
 
-for x in range(5):
-    threads.append(MyThread("task1", str(x)).start())
-    for t in threading.enumerate():
-        print("name=", t.getName())
+def service_shutdown(signum, frame):
+    print('Caught signal %d' % signum)
+    raise ServiceExit
 
 
-size = len(threads) - 1
-for x in range(size, -1, -1):
-    print("x=", x)
-    if(threads[x]):
-        threads[x].join()
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+streamHandler = logging.StreamHandler()
+streamHandler.setFormatter(formatter)
+log1 = logging.getLogger('Cam_manager')
+fileHandler1 = logging.FileHandler('log/camera_manager.log', mode='a+')
+fileHandler1.setFormatter(formatter)
+log1.setLevel("DEBUG")
+log1.addHandler(fileHandler1)
+log1.addHandler(streamHandler)
+
+log1.info("start main cam manager")
+
+filename = rootPath = os.path.dirname(os.path.abspath(__file__)) + "/config/camConfig.json"
+try:
+    with open(filename, 'r') as fh:
+        data = fh.read()
+    config = json.loads(data)
+    cameras = config["cameras"]
+    log1.info("Successful load config file.")
+    for cam in cameras:
+        url = cam + cameras[cam]["Port"]
+        if(cameras[cam]["Recognation"].lower() == "true"):
+            log1.info("Try open camera " + url)
+            if(cameras[cam]["Port"] == ""):
+                url = int(cam)
+            name = cameras[cam]["Name"]
+            c = camera.Camera(name, url)
+            c.start()
+            threads.append(c)
+            # camera("rtsp://name:pass@192.168.1.108:554")
+except Exception as e:
+    print("error open file", e)
+
+try:
+    while 1:
+        pass
+        # print("main running ", len(threads), threads[0].name)
+
+except KeyboardInterrupt:
+    size = len(threads) - 1
+    log1.info("\n Stopping active cameras: " + str(size))
+    for x in range(size, -1, -1):
+        print("x=", x, threads[x].name)
+        if(threads[x]):
+            # print("kill", threads[x].name)
+            threads[x]._stopevent.set()
+            threads[x].stop()
         del threads[x]
-    else:
-        del threads[x]
-print("sum=", len(threads))
+    log1.info("End keyboard stop main cam manager")
--- camera.py
+++ camera.py
@@ -11,33 +11,38 @@ import sys
 class Camera(threading.Thread):
     def __init__(self, name, url):
         self.cameraName = name
+        self.log1 = logging.getLogger('Camera_' + self.cameraName)
         self.url = url
-        self.log1 = ""
         self.cap = 0
+        self.delay_before_add = 2  # number of frames with face before add
+        self.delay_before_leave = 5  # number of frames with face before leave
         self.ramp_frames = 20
         self.location = os.getcwd()  # get present working directory location here
-        self.known_face_encodings = []
-        self.known_face_names = []
-        self.detected_faces = []
+        self._stopevent = threading.Event()
         threading.Thread.__init__(self)
 
     def run(self):
+        self.known_face_encodings = []
+        self.known_face_names = []
+        self.detected_faces = {}
+        self.detected_faces_new = {}
+        self.unknown_face_encodings = {}
+        self.unknown_face_names = []
+        self.unknown_face_imgs = []
+        # self.detected_faces_unknown = {}
         formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
         streamHandler = logging.StreamHandler()
         streamHandler.setFormatter(formatter)
-
-        self.log1 = logging.getLogger('Camera_' + self.cameraName)
         fileHandler1 = logging.FileHandler('log/camera_' + self.cameraName + '.log', mode='a+')
         fileHandler1.setFormatter(formatter)
-        self.log1 = logging.getLogger('Camera_' + self.cameraName)
         self.log1.setLevel("DEBUG")
         self.log1.addHandler(fileHandler1)
         self.log1.addHandler(streamHandler)
 
         self.log2 = logging.getLogger('Camera_face_' + self.cameraName)
-        self.log2.setLevel("DEBUG")
         fileHandler2 = logging.FileHandler('log/camera_face_' + self.cameraName + '.log', mode='a+')
         fileHandler2.setFormatter(formatter)
+        self.log2.setLevel("DEBUG")
         self.log2.addHandler(fileHandler2)
         self.log2.addHandler(streamHandler)
 
@@ -59,94 +64,126 @@ class Camera(threading.Thread):
         start_counter = 4
         counter = start_counter
         self.log1.info("start camera " + str(self.url))
-        self.log2.info("start camera " + str(self.url))
         self.cap = cv2.VideoCapture(self.url)
-        self.log1.debug("opened=" + str(self.cap.isOpened()))
         if(self.cap.isOpened()):
-            while(True):
-                if(counter > 0):
-                    counter -= 1
-                else:
-                    counter = start_counter
-                    ret, frame_sm = self.cap.read()
-                    if(ret):
-                        # frame_sm = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-                        face_locations = face_recognition.face_locations(frame_sm)
-                        face_encodings = face_recognition.face_encodings(frame_sm, face_locations)
-                        face_names = []
-                        # print("faces:", len(face_locations))
-                        if (len(face_locations) > 0):
-                            for face_encoding, face_location in zip(face_encodings, face_locations):
-                                # See if the face is a match for the known face(s)
-                                matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding, 0.6)
-                                name = "unknown_"
-                                top, right, bottom, left = face_location
-                                # print("face location:", left, top, right, bottom)
-                                if True in matches:
-                                    # facesFounded = True
-                                    first_match_index = matches.index(True)
-                                    name = self.known_face_names[first_match_index]
+            self.log2.info("started camera " + str(self.url))
+            try:
+                while not self._stopevent.isSet():
+                    if(counter > 0):
+                        counter -= 1
+                    else:
+                        counter = start_counter
+                        ret, frame_sm = self.cap.read()
+                        if(ret):
+                            # frame_sm = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+                            face_locations = face_recognition.face_locations(frame_sm)
+                            face_encodings = face_recognition.face_encodings(frame_sm, face_locations)
+                            face_names = []
+                            # print("faces:", len(face_locations))
+                            if (len(face_locations) > 0):
+                                for face_encoding, face_location in zip(face_encodings, face_locations):
+                                    # See if the face is a match for the known face(s)
+                                    matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding, 0.6)
+                                    name = ""
+                                    if True in matches:
+                                        first_match_index = matches.index(True)
+                                        name = self.known_face_names[first_match_index]
+                                    else:
+                                        # new face is detected
+                                        name = "unknown_" + str(random.randint(100000, 1000000)) + "_0"
+                                        if(len(self.unknown_face_encodings) > 0):
+                                            matches_u = face_recognition.compare_faces(self.unknown_face_encodings, face_encoding, 0.6)
+                                            if True in matches_u:
+                                                first_match_index_u = matches_u.index(True)
+                                                name = self.unknown_face_names[first_match_index_u]
+                                                # save photo if present during several frames !!!!!!
+                                                self.detected_faces_new[name] = self.detected_faces_new[name] - 1
+                                                if(self.detected_faces_new[name] == 0):
+                                                    top, right, bottom, left = face_location
+                                                    delta = 40
+                                                    height, width, channels = frame_sm.shape
+                                                    if (top - delta < 0):
+                                                        top = 0
+                                                    else:
+                                                        top = top - delta
+                                                    if (left - delta < 0):
+                                                        left = 0
+                                                    else:
+                                                        left = left - delta
+                                                    if (bottom + delta > height):
+                                                        bottom = height
+                                                    else:
+                                                        bottom = bottom + delta
+                                                    if (right + delta > width):
+                                                        right = width
+                                                    else:
+                                                        right = right + delta
+                                                    abs_file_path_save = os.path.join(self.location, "faces/" + name + ".png")
+                                                    crop_img = frame_sm[top:bottom, left:right]
+                                                    height_sm, width_sm, channels_sm = crop_img.shape
+                                                    if(height_sm > 0 and width_sm > 0):
+                                                        cv2.imwrite(abs_file_path_save, crop_img)
+                                                    else:
+                                                        self.log1.error("Error create file with face")
+                                            else:
+                                                self.unknown_face_encodings.append(face_encoding)
+                                                self.unknown_face_names.append(name)
+                                                self.detected_faces_new[name] = self.delay_before_add
+                                                self.log2.info("Done add unknown face " + name)
+                                        else:
+                                            self.unknown_face_encodings.append(face_encoding)
+                                            self.unknown_face_names.append(name)
+                                            self.detected_faces_new[name] = self.delay_before_add
+                                            self.log2.info("Done add unknown face " + name)
+                                    face_names.append(name)
                                     if(not name in self.detected_faces):
                                         self.log2.info("Detect face " + name)
-                                        self.detected_faces.append(name)
-                                else:
-                                    delta = 40
-                                    height, width, channels = frame_sm.shape
-                                    if (top - delta < 0):
-                                        top = 0
+                                    self.detected_faces[name] = self.delay_before_leave
+                                    # Draw a label with a name below the face
+                                    # cv2.rectangle(frame_sm, (left, top), (right, bottom), (0, 0, 255), 2)
+                                    # cv2.rectangle(frame_sm, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                                    # font = cv2.FONT_HERSHEY_DUPLEX
+                                    # cv2.putText(frame_sm, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+                                del_names = []
+                                for key, value in self.detected_faces.items():
+                                    if (not key in face_names):
+                                        if(self.detected_faces[key] > 0):
+                                            self.detected_faces[key] = value - 1
+                                        else:
+                                            del_names.append(key)
+                                for i in del_names:
+                                    del self.detected_faces[i]
+                                    self.log2.info("Leave face: " + i)
+                                del_names2 = []
+                                for key, value in self.detected_faces_new.items():
+                                    if (not key in face_names):
+                                        del_names2.append(key)
+                                for j in del_names2:
+                                    del self.detected_faces_new[j]
+                                    self.log2.info("remove unknow face: " + j)
+                            else:
+                                del_names = []
+                                for key, value in self.detected_faces.items():
+                                    if(self.detected_faces[key] > 0):
+                                        self.detected_faces[key] = value - 1
                                     else:
-                                        top = top - delta
-                                    if (left - delta < 0):
-                                        left = 0
-                                    else:
-                                        left = left - delta
-                                    if (bottom + delta > height):
-                                        bottom = height
-                                    else:
-                                        bottom = bottom + delta
-                                    if (right + delta > width):
-                                        right = width
-                                    else:
-                                        right = right + delta
-                                    dig = str(random.randint(10000, 1000000))
-                                    name = "unknown_" + dig + "_0"
-                                    abs_file_path_save = os.path.join(self.location, "faces/" + name + ".png")
-                                    crop_img = frame_sm[top:bottom, left:right]
-                                    height_sm, width_sm, channels_sm = crop_img.shape
-                                    if(height_sm > 0 and width_sm > 0):
-                                        cv2.imwrite(abs_file_path_save, crop_img)
-                                    else:
-                                        self.log1.error("Error create file with face")
-                                    self.known_face_encodings.append(face_encoding)
-                                    self.known_face_names.append(name)
-                                    self.log2.info("Done add new face " + name)
-                                face_names.append(name)
-                                cv2.rectangle(frame_sm, (left, top), (right, bottom), (0, 0, 255), 2)
-                                # Draw a label with a name below the face
-                                cv2.rectangle(frame_sm, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-                                font = cv2.FONT_HERSHEY_DUPLEX
-                                cv2.putText(frame_sm, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
-                            for n in self.detected_faces:
-                                if (not n in face_names):
-                                    self.detected_faces.remove(n)
-                                    self.log2.info("Leave face " + n)
+                                        del_names.append(key)
+                                for i in del_names:
+                                    del self.detected_faces[i]
+                                    self.log2.info("Leave face: " + i)
+                                self.detected_faces_new.clear()
+                            # cv2.imshow(self.cameraName, frame_sm)
                         else:
-                            if(len(self.detected_faces) > 0):
-                                self.log2.info("Leave faces " + ",".join(self.detected_faces))
-                                del self.detected_faces[:]
-                        cv2.imshow(self.cameraName, frame_sm)
-                    else:
-                        self.log1.error("can not connect ot camera")
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    self.stop()
-                    break
-            self.stop()
+                            self.log1.error("can not connect ot camera")
+            except Exception as e:
+                print("stop camera", e)
+                # self.stop()
         else:
             self.log1.error("no cameras")
 
     def stop(self):
-        self.log1.info("stop camera " + self.cameraName)
-        self.log2.info("stop camera " + self.cameraName)
+        self.log1.info("stop 1 camera " + self.cameraName)
+        self.log2.info("stop 2 camera " + self.cameraName)
         if(self.cap):
             self.cap.release()
         cv2.destroyAllWindows()
@@ -154,6 +191,4 @@ class Camera(threading.Thread):
         self.join()
 
 
-camera = Camera("webCam_0", 0).start()
-
-# start_camera("rtsp://os:Bluher11_@192.168.1.108:554")
+Camera("test1", 0).start()
